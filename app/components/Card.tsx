95626a2b633591d9e581f5a899c9d570c84c788c
neenukurian92@gmail.com <neenukurian92@gmail.com>
2025-12-15 13:44:01 +0100

    style: improve styling of cards

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -2,32 +2,36 @@ import { Show } from "../types/index.ts";
 import MovieRating from "./MovieRating";
 import Image from "next/image";
 import { BiImage } from "react-icons/bi";
+import { sanitiseText } from "../utilities/sanitiseText.ts";
 
-const Card = ({ name, rating, image, index }: Show & { index: number }) => {
+const Card = ({ name, rating, image, index, summary }: Show & { index: number }) => {
   return (
-    <div>
+    <div className="border-gray-300 border rounded-md">
       <div className="bg-white relative h-[400px] w-[250px] rounded-lg shadow-lg overflow-hidden shrink-0">
         {image?.original ? (
           <Image
             src={image?.original}
             alt={name || "No image available"}
             fill
-            className="object-cover"
+            className="object-cover hover:scale-105 transition-transform duration-300"
             unoptimized
             priority={index < 6}
           />
         ) : (
           <BiImage aria-hidden="true" className="object-cover" size={250} />
         )}
-        <div className="absolute top-4 left-4 z-10">
+        <div className="absolute top-4 left-4 z-10 bg-black/80 py-1 px-2 rounded-md">
           {rating?.average && (
             <MovieRating rating={rating?.average * 10 || 0} />
           )}
         </div>
       </div>
-      <h2 className="text-xl font-bold text-black my-3 truncate max-w-[250px] text-wrap">
+      <h2 className="text-xl font-medium text-gray-900 my-3 truncate max-w-[250px] text-wrap px-2">
         {name}
       </h2>
+      <p className="text-gray-600 my-3 truncate max-w-[250px] text-wrap px-2 text-md">
+        {sanitiseText(summary)?.slice(0, 100)}...
+      </p>
     </div>
   );
 };
